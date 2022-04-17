355826eccbec678185d6a31ab9c3ccf7af3e9af2
Eva Pace <eba.pachi@gmail.com>
2022-04-16 23:24:14 -0300

    repl: Use rustyline for main loop

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,3 +1,31 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("☀️  praise the sun \\o/");
+    // TODO: add completer by replacing `()`
+    let mut rl = Editor::<()>::new();
+    // TODO: make history more robust?
+    let _ = rl.load_history("history.txt");
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
 }
