43df0f24633787f21b64d0a521a799fe50f9ba71
Eva Pace <eba.pachi@gmail.com>
2022-04-16 23:51:22 -0300

    lang: Add similar lua_dostring func

--- lang/src/lib.rs
+++ lang/src/lib.rs
@@ -1,3 +1,13 @@
 mod state;
 
 pub use state::State;
+
+/// Creates a Sol state, executes a line then kills it.
+// lua_dostring
+pub fn exec_line(line: &str) {
+    let state = state::State::new();
+    state.load_std();
+
+    state.load_line(line);
+    state.call();
+}
